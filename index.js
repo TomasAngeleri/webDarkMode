aa4e7510168a866ad2d700e296b671b47f2ed6f7
TomasAngeleri <tomi.angeleri94@gmail.com>
2021-01-27 20:07:14 -0300

    End web site dark mode

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hola");
+const btnMode = document.getElementById("btnMode");
+
+//Obtengo la config del user
+const configUser = window.matchMedia('(prefers-color-scheme: dark)')
+
+const localConfig = localStorage.getItem('tema');
+if(localStorage === 'dark'){
+    document.body.classList.toggle('dark-theme');
+}else if(localStorage ==='light'){
+    document.body.classList.toggle('light-theme');
+}
+
+btnMode.addEventListener('click',()=>{
+    let colorTema;
+    if(configUser.matches){
+        document.body.classList.toggle('light-theme')
+        colorTema = document.body.classList.contains('light-theme') ? 'light ' : 'dark';
+    }else{
+        document.body.classList.toggle('dark-theme')
+        colorTema = document.body.classList.contains('dark-theme') ? 'dark ' : 'light';
+    }
+    localStorage.setItem('tema',colorTema)
+})
